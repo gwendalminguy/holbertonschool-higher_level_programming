6e1ed4808a54913ac7353f856a924d01dca3216a
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-06 13:24:51 +0200

    Corrected message to print

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 
 def main():
-    total = len(sys.argv) - 1
+    n = 1
+    total = len(argv) - 1
 
     if total == 0:
-        print("0 argument.")
+        print("0 arguments.")
+
     elif total == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
+        print("1: {}".format(argv[1]))
+
     else:
         print("{} arguments:".format(total))
-        n = 1
-        for i in sys.argv[1:]:
+        for i in argv[1:]:
             print("{}: {}".format(n, i))
             n += 1
 
