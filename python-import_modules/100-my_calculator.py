74d0862f09338e3b9f3e82de8a9f3f4328b167ca
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-07 12:57:12 +0200

    Created script computingg command-line arguments

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+
+def main():
+    if len(argv) != 4:
+        print(len(argv))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    match op:
+        case '+':
+            result = add(a, b)
+        case '-':
+            result = sub(a, b)
+        case '*':
+            result = mul(a, b)
+        case '/':
+            result = div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, result))
+
+
+if __name__ == "__main__":
+    main()
