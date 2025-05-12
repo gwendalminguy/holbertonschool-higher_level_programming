93f31856f9992bb41b23adedc278a082f65aad71
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-12 14:09:28 +0200

    Created function replacing an element by another in a list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if search in my_list:
+        new = my_list.copy()
+        for i in range(len(new)):
+            if new[i] == search:
+                new[i] = replace
+    return new
