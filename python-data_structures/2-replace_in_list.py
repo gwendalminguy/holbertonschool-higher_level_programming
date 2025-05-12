61f86150834a1298612ad884e27c997ffa2c2e1b
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-12 10:07:52 +0200

    Correct variable name

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -6,5 +6,16 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list
     else:
-        my_lis[idx] = element
+        my_list[idx] = element
         return my_list
+
+def main():
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = replace_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
+
+main()
