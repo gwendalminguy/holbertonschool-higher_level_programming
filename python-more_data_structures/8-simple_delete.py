adc27058bad69c0c1b9cfb62976d86fa2ce0fdc8
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-12 15:56:58 +0200

    Corrected with pycodestyle

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if not key in a_dictionary.keys():
+    if key not in a_dictionary.keys():
         return a_dictionary
     else:
         a_dictionary.pop(key)
