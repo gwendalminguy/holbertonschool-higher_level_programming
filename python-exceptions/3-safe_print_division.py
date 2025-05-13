ba7d3e5283057b3c341c9e9ec377d0bb54002250
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-13 13:27:25 +0200

    Removed main function

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,16 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
-def main():
-    a = 12
-    b = 2
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-    a = 12
-    b = 0
-    result = safe_print_division(a, b)
-    print("{:d} / {:d} = {}".format(a, b, result))
-
-main()
