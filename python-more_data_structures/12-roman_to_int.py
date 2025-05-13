22ae1d14cc274136ba476d7301841ba1cb69c2ea
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-13 09:40:11 +0200

    Corrected case where roman_string is not a string or is None

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    elif not isinstance(roman_string, string):
+        return 0
+
     result = 0
     num = 0
     roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
