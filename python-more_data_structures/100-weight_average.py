537a35dbb209ab5485307d81296516e9cbdb0519
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-13 10:15:19 +0200

    Change return type to float

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
-        return 0
     result = 0
     count = 0
+    if len(my_list) == 0:
+        return result
     for couple in my_list:
         result += couple[0] * couple[1]
         count += couple[1]
 
-    return (result / count)
+    return (float(result / count))
+
+def main():
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
+
+main()
