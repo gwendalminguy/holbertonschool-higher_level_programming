2e56ce81d3c394ba4ca31b39b2d525724118384b
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-13 09:57:26 +0200

    Removed main function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -10,13 +10,3 @@ def best_score(a_dictionary):
             highest = a_dictionary[key]
             name = key
     return name
-
-def main():
-    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-    best_key = best_score(a_dictionary)
-    print("Best score: {}".format(best_key))
-
-    best_key = best_score(None)
-    print("Best score: {}".format(best_key))
-
-main()
