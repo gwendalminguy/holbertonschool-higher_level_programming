f79470b5de6da3190e2d96ab6abe2c5e091ad01f
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-15 14:19:35 +0200

    Added function and module documentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+
+"""
+4-print_square.py
+Module containing a function printing a square.
+"""
+
+
 def print_square(size):
+    """
+    Prints a square.
+
+    Parameters:
+    size (int): size of the square
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
