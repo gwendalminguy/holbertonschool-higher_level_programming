54ccc599a411b43d57f03607c440b131c3ff9a21
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-15 14:16:41 +0200

    Added function and module documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+
+"""
+5-text_indentation.py
+Module containing a function changing some text.
+"""
+
+
 def text_indentation(text):
+    """
+    Adding blank lines in some text.
+
+    Parameters:
+    text (str): text to change
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
