bd4923e1b371b49010e7b243dec0b477fb5c45a6
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-19 20:10:23 +0200

    Corrected case where width or height is zero

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -56,4 +56,6 @@ class Rectangle:
         """
         Computes the perimeter of the rectangle.
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * self.__width + 2 * self.__height
