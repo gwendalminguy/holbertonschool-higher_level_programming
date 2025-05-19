231585b9a683b9cc9a406b32ba048aeca5409767
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-19 16:36:01 +0200

    Corrected with pycodestyle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
