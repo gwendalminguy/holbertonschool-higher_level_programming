c1d164a431e978526ffaaeb10fac8c7b960e48dc
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-19 10:45:36 +0200

    Created class Square with attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+1-square.py
+Module containing classe Square.
+"""
+
+
+class Square:
+    """
+    A class to define a square.
+    """
+    def __init__(self, size):
+        self.__size = size
