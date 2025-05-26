aafc546c4f5c058cd9c45470a25481964ce74ff5
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-26 13:07:57 +0200

    Completed documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Module containing ...
+Module containing the function inherits_from.
 """
 
 
 def inherits_from(obj, a_class):
     """
-    ...
+    Checks if an object inherits from a class.
     """
     if type(obj) is a_class:
         return False
