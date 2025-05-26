c894ef283ce3b7d6a83dced2d735757fc918e10f
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-26 13:34:30 +0200

    Created class BaseGeometry with method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module containing the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    A class to define a base geometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
