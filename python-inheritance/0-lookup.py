6e1f8eb07757015cba1bc648834adb71ebd6186a
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-26 10:27:07 +0200

    Created function getting all properties and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-lookup.py
+Module containing the function lookup.
+"""
+
+
+def lookup(obj):
+    """
+    Gets all properties and methods of an object.
+    """
+    return dir(obj)
