7210a0e5806132a22156bfb1be8871e5b871c483
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-26 10:56:13 +0200

    Completed documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
 1-my_list.py
-Module containing ...
+Module containing class MyList.
 """
 
 
 class MyList(list):
+    """
+    A class to define list methods.
+    """
     def print_sorted(self):
         sorted = self.copy()
         sorted.sort()
