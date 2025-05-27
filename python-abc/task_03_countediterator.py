221b8a4e000ca28dc71e99058ce653449b9f5d4f
gwendalminguy <gwendalminguy@outlook.fr>
2025-05-27 16:06:34 +0200

    Removed unused attribute length

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -11,7 +11,6 @@ class CountedIterator:
     def __init__(self, data):
         self.iterator = iter(data)
         self.counter = 0
-        self.length = len(data)
 
     def __next__(self):
         self.counter += 1
