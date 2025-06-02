ac8ebc5f491c83490dee407c17c07d31b726e9ae
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-02 23:51:53 +0200

    Created class Student with method to_json

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+10-student.py
+Module containing class Student.
+"""
+
+
+class Student:
+    """
+    A class to define a student.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        for element in attrs:
+            if not isinstance(element, str):
+                return self.__dict__
+
+        dictionary = {}
+        if "first_name" in attrs:
+            dictionary["first_name"] = self.first_name
+        if "last_name" in attrs:
+            dictionary["last_name"] = self.last_name
+        if "age" in attrs:
+            dictionary["age"] = self.age
+        return dictionary
