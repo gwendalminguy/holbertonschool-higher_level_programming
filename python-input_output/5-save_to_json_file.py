99a5b7718b2baaf837171e3fbee5c47ec46b9a58
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-02 13:17:22 +0200

    Created function writing an object as a JSON to a file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+5-save_to_json_file.py
+Module containing function to write an object as a JSON to a file.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Function creating writing an object as a JSON to a file.
+    """
+    string = json.dumps(my_obj)
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(string)
