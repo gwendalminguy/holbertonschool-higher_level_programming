225e857c6e19d247f801989478c21a089095e96b
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-03 19:54:04 +0200

    Added return to save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_json_file(my_obj, filename):
     """
     string = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(string)
+        return f.write(string)
