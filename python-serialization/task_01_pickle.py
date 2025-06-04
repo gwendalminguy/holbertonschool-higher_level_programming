7e0fb043c57212594aff6e8f5bb002433757e364
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-04 13:03:10 +0200

    Created class CustomObject with serialize and deserialize methods

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module containing class CustomObject
+"""
+import pickle
+
+
+class CustomObject:
+    """
+    Class defining a custom object.
+    """
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as f:
+            obj = pickle.load(f)
+
+        return obj
