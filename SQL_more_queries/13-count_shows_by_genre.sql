883b816dc0349a3ec225ae107702f33e65abe27a
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-24 11:40:47 +0200

    Corrected column name for genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all genres in hbtn_0D_tvshows with number of shows linked
-SELECT name, COUNT(show_id) AS number_of_shows
+SELECT name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
