170e9fa1dd83f8bccfeaad2fc8d0934b89fb9e7d
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-24 12:34:53 +0200

    Added order by name

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter' AND tv_genres.id IS NOT NULL;
+WHERE tv_shows.title = 'Dexter' AND tv_genres.id IS NOT NULL
+ORDER BY name;
