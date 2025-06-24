0a352f0e0ba89d574a2ae06378fd1b467019a067
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-24 10:07:11 +0200

    Created script listing all cities contained in a database

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Lists all the cities contained in database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name FROM states
+INNER JOIN cities
+ON states.id = cities.state_id
+ORDER BY cities.id ASC;
