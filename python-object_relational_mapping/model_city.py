d49c0d967f034ad292de72d59f9b8fa5eb014030
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-26 08:49:01 +0200

    Added City class

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module containing class City.
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    Class defining a city.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    
+    state = relationship('State', backref='cities')
