a3cd19d91e4df6cb47b2353f939a24c839fcee86
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-26 08:49:23 +0200

    Created script listing all cities with state name

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,47 @@
+#!./venv/bin/python3
+"""
+Module containing function listing objects from a database.
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import select
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def main():
+    """
+    Lists State objects containing 'a' from a database.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username,
+            password,
+            database
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    results = session.execute(
+        select(City, State).join(State, City.state_id == State.id)
+    )
+
+    for row in results:
+        print("{}: ({}) {}".format(row.State.name, row.City.id, row.City.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
