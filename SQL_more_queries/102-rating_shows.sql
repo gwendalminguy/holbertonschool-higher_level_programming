03f7f76a9461e82cee923e30a487ec0d28464201
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-27 12:07:17 +0200

    Corrected with SQL style

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- Lists all shows ordered by rating in hbtn_0d_tvshows_rate
-SELECT title, SUM(rate) as rating FROM tv_shows
+SELECT title, SUM(rate) AS rating FROM tv_shows
 LEFT JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
