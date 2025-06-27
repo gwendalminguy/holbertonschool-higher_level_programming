dde826b11eac7d25c5a2181c07aef99193c140c0
gwendalminguy <gwendalminguy@outlook.fr>
2025-06-27 12:00:59 +0200

    Created script listing all shows ordered by rate sum in a database

---
+++ SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows ordered by rating in hbtn_0d_tvshows_rate
+SELECT title, SUM(rate) as rate FROM tv_shows
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
+ORDER BY rate DESC;
