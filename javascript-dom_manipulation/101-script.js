1493066e6f5d4ee856e239fb4f10112a263e5cf9
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-15 15:49:37 +0200

    Created script changing greeting according to selected language

---
+++ javascript-dom_manipulation/101-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector('#btn_translate');
+
+  button.addEventListener('click', () => {
+    const language = document.querySelector('#language_code').value;
+    const url = `https://hellosalut.stefanbohacek.com/?lang=${language}`;
+    fetch(url).then( function (response) {
+      return response.json();
+    }).then(function (data) {
+      document.querySelector('#hello').textContent = data.hello;
+    });
+  });
+});
