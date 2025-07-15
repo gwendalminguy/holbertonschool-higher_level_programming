0fc9109da4137525be2790ca94cc37d1bfb09eae
gwendalminguy <gwendalminguy@outlook.fr>
2025-07-15 15:50:20 +0200

    Corrected with semistandard

--- javascript-dom_manipulation/101-script.js
+++ javascript-dom_manipulation/101-script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   button.addEventListener('click', () => {
     const language = document.querySelector('#language_code').value;
     const url = `https://hellosalut.stefanbohacek.com/?lang=${language}`;
-    fetch(url).then( function (response) {
+    fetch(url).then(function (response) {
       return response.json();
     }).then(function (data) {
       document.querySelector('#hello').textContent = data.hello;
